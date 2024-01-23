9227626eb5824fd0c6b2f42dcb4a77fd247c337c
Kassandra Sanchez <kassandrasmedina@gmail.com>
2024-01-22 23:16:16 -0800

    render the puppy data

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,31 @@ import { puppyList } from './data.js'
 
 function App() {
   const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
 
-  console.log("puppyList: ", puppyList);
-
+  //console.log("puppyList: ", puppyList);
+  
+  const featuredPup = puppies.find((pup)=> pup.id === featPupId);
+  //console.log(featuredPup);
   return (
     <div>
+      <h1>Puppy Pals</h1>
       { 
         puppies.map((puppy) => {
-        return <p>{puppy.name}</p>
+          return <p onClick={()=>{ setFeatPupId(puppy.id)}} key={puppy.id}>{puppy.name}</p>
+          
         })
       }
+      { featPupId && (
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+            <li>Owner Id: {featuredPup.ownerId}</li>
+          </ul>
+        </div>
+      )}
     </div>
   );
 }
